109262be4e75b723d293eab624211869e6c051e3
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2024-07-14 22:30:04 +0530

    dad jokes api complete

---
+++ dad-jokes-api/script.js
@@ -0,0 +1,19 @@
+const joke = async () => {
+  try {
+    const response = await fetch("https://icanhazdadjoke.com/", {
+      headers: {
+        Accept: "application/json",
+      },
+    });
+    const json = await response.json();
+    // adding the json response of the api to the jokes-container inner text
+    document.getElementById("jokes-container").innerText = json.joke;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// adding event listener to button and calling the joke function
+document.getElementById("btn").addEventListener("click", () => {
+  joke();
+});
